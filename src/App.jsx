c6b30fbe1161d9726e6423bf0650d33e57f57948
ANGlenn <fghernandez108@gmail.com>
2024-04-07 06:05:15 +0800

    Fix mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import header from './assets/header.png'
+import footer from './assets/footer.png'
 import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className='grid'>
-      <div id='background' className='row-start-1 col-start-1 w-full h-full flex '>
-      </div>
-      <h1 className='row-start-1 col-start-1 w-full h-18 text-center pt-1 text-4xl'>Summasabit ka pa ba?</h1>
-      <div className='w-full h-screen flex justify-center row-start-1 col-start-1'>
-        <div className='sm:w-10/12 w-full flex h-96 sm:h-auto mt-36 sm:mt-24'>
+    <div id='App' className='flex-col justify-between'>
+      <header className='h-12 sm:h-24 sm:-translate-y-44 sm:w-full grid'>
+        <img className='row-start-1 col-start-1 w-full' src={header}></img>
+        <h1 className='row-start-1 col-start-1 w-full h-18 text-center pt-1 text-3xl sm:text-4xl sm:font-black'>Summasabit ka pa ba?</h1>
+      </header>
+      <h1 className='opacity-0 sm:opacity-100 row-start-1 col-start-1 w-full h-18 text-center pt-1 text-3xl sm:text-4xl'>Summasabit ka pa ba?</h1>
+      
+      <div className='w-ful flex justify-center row-start-1 col-start-1 sm:h-96'>
+        <div className='sm:w-10/12 w-full flex mt-24 sm:mt- sm:-translate-y-28'>
           <Calculator/>
         </div>
       </div>
+
+      <footer className='w-full h-24 sm:h-52 overflow-hidden sm:w-full'>
+        <img src={footer} className='w-full sm:-translate-y-44 '></img>
+      </footer>
+
     </div>
   )
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -47,42 +47,42 @@ const Calculator = () => {
   }
 
   return (
-    <div className='flex flex-col items-center sm:items-start sm:flex-row'>
-      <div id="Card1" className='h-96 w-10/12 sm:w-auto flex rounded-t-3xl sm:rounded-r-none sm:rounded-l-3xl'>
+    <div className='flex flex-col items-center sm:items-start sm:flex-row sm:h-96'>
+      <div id="Card1" className='w-10/12 sm:w-auto flex rounded-t-3xl sm:rounded-r-none sm:rounded-l-3xl sm:h-full'>
         <form action='' className='py-3 px-8 sm:p-10 flex-col flex justify-between gap-5 w-full'>
-          <h2 className='mb-1 sm:m-0 w-full rounded-3xl text-2xl sm:text-3xl instructions'>Insert values here</h2>
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-xl sm:text-4xl' for='ut'>Units Taken</label>
-            <input onChange={changeHandler} className='rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={200} name='ut' type='number'></input>  
+          <h2 className='mb-1 sm:m-0 w-full rounded-3xl text-xl sm:text-3xl instructions'>Insert values here</h2>
+          <div className='flex justify-between items-center'>
+            <label className='text-xl sm:text-4xl w-1/2' for='ut'>Units Taken</label>
+            <input onChange={changeHandler} className='w-1/2 rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={200} name='ut' type='number'></input>  
           </div>
 
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-xl sm:text-4xl' for='ul'>Units left</label>
-            <input onChange={changeHandler} className='rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={200} name='ul' type='number'></input>  
+          <div className='flex justify-between items-center'>
+            <label className='text-xl sm:text-4xl w-1/2 pr-3' for='ul'>Units left</label>
+            <input onChange={changeHandler} className='w-1/2 rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={200} name='ul' type='number'></input>  
           </div>
 
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-xl sm:text-4xl' for='gwa'>Current GWA</label>
-            <input onChange={changeHandler} className='rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={5.0} step={0.0001} name='gwa' type='number'></input>  
+          <div className='flex justify-between items-center'>
+            <label className='text-xl sm:text-4xl w-1/2' for='gwa'>Current GWA</label>
+            <input onChange={changeHandler} className='w-1/2 rounded-3xl px-4 text-xl sm:text-3xl' min={1.0} max={5.0} step={0.0001} name='gwa' type='number'></input>  
           </div>
         </form>
       </div>
-      <div id="Card2" className='h-96 w-10/12 sm:w-auto flex rounded-b-3xl sm:rounded-l-none sm:rounded-r-3xl'>
-        <form className='py-3 px-8 sm:p-10 flex-col flex justify-between gap-5 w-full'>
-          <h2 className='w-full rounded-3xl text-xl px-8 sm:px-0 sm:text-2xl instructions'>Average grade of remaining units needed</h2>
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-lg sm:text-4xl' for='ut'>Summa Cum laude</label>
-            <h2 className='avg rounded-3xl text-2xl'>{(isFinite(summa)? (summa<1? "impossible" : (summa>3? 3 : summa)) : null)}</h2> 
+      <div id="Card2" className='w-10/12 sm:w-auto flex rounded-b-3xl sm:rounded-l-none sm:rounded-r-3xl sm:h-full'>
+        <form className='py-4 px-4 sm:p-10 flex-col flex justify-between gap-5 w-full'>
+          <h2 className='w-full rounded-3xl text-lg h-22 px-8 sm:px-0 sm:text-2xl instructions'>Average grade of remaining units needed</h2>
+          <div className='flex justify-between items-center'>
+            <label className='text-lg sm:text-4xl w-1/2' for='ut'>Summa Cum laude</label>
+            <h2 className='avg rounded-3xl text-2xl w-1/2'>{(isFinite(summa)? (summa<1? "impossible" : (summa>3? 3 : summa)) : null)}</h2> 
           </div>
 
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-lg sm:text-4xl' for='ul'>Magna Cum laude</label>
-            <h2 className='avg rounded-3xl text-2xl'>{(isFinite(magna)? (magna<1? "impossible" : (magna>3? 3 : magna)) : null)}</h2>  
+          <div className='flex justify-between items-center'>
+            <label className='text-lg sm:text-4xl w-1/2' for='ul'>Magna Cum laude</label>
+            <h2 className='avg rounded-3xl text-2xl w-1/2'>{(isFinite(magna)? (magna<1? "impossible" : (magna>3? 3 : magna)) : null)}</h2>  
           </div>
 
-          <div className='flex gap-3 justify-between items-center'>
-            <label className='text-lg sm:text-4xl' for='gwa'>Cum laude</label>
-            <h2 className='avg rounded-3xl text-2xl'>{(isFinite(cum)? (cum<1? "impossible" : (cum>3? 3 : cum)) : null)}</h2>  
+          <div className='flex justify-between items-center'>
+            <label className='text-lg sm:text-4xl w-1/2' for='gwa'>Cum laude</label>
+            <h2 className='avg rounded-3xl text-2xl w-1/2'>{(isFinite(cum)? (cum<1? "impossible" : (cum>3? 3 : cum)) : null)}</h2>  
           </div>
         </form>
     </div>
